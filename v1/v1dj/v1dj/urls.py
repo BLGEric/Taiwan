ab9205f1f6a863b4740cabdf4071887b56427292
uu <apple@D3-0805-5.dorm.ntust.edu.tw>
2016-06-10 21:14:16 +0800

    1

--- v1/v1dj/v1dj/urls.py
+++ v1/v1dj/v1dj/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 """v1dj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,6 +15,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
